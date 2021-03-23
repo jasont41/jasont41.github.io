fa4a04e3c00d3c22e8949349ae64887df86606f0
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-03-23 02:23:54 -0700

    Need to stall code to give time for API call to finish

--- onloadScript.js
+++ onloadScript.js
@@ -1,33 +1,35 @@
 var repoNames = [];
-var nHTML = "";
+var nHTML = '';
+var count = 0; 
 window.onload = function(){
 	
 	var request = new XMLHttpRequest(); 
- 
+	
 	request.open('GET', 'https://api.github.com/users/jasont41/repos', true) ;
- 
+ 	
 	
 	request.onload = function(){
 		var data = JSON.parse(this.response)
- 
+		
 		if (request.status >= 200 && request.status < 400) 
 			{
 				data.forEach((repo) => { 
-				repoNames.push(repo.name); 
+				repoNames.push((repo.name)); 
 				console.log(repo.name);
-				//document.getElementById("demo").innerHTML += repo.name+"<br>"; 
+				count++; 
 				});
 		}
 		else{ console.log('error');}
 	}
-	console.log(repoNames.length);
-	
-	var i = 0; 
-	for(i; i < 17; i++){
-	document.getElementById("demo").innerHTML +=repoNames[i]; 
-	}
-	 nHTML; 
-	request.send();
-	
+	//need to find a way to wait 
 
+	console.log(repoNames[5]);
+	var i = 0; 
+	
+	request.send();
+}
+
+function testing(){
+	for(var i = 0; i< count; i++)
+		document.getElementById("demo").innerHTML += repoNames[i] + "<br>"; 
 }
