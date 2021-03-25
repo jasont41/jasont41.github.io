1de75c0d0a271fba62461eeb444a5ff4ee23422c
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-03-25 01:01:00 -0700

    Images loading from api call.
    Cool stuff

--- onloadScript.js
+++ onloadScript.js
@@ -4,13 +4,9 @@ var count = 0;
 window.onload = function(){
 	
 	var request = new XMLHttpRequest(); 
-	
 	request.open('GET', 'https://api.github.com/users/jasont41/repos', true) ;
- 	
-	
 	request.onload = function(){
-		var data = JSON.parse(this.response)
-		
+		var data = JSON.parse(this.response)	
 		if (request.status >= 200 && request.status < 400) 
 			{
 				data.forEach((repo) => { 
@@ -21,15 +17,18 @@ window.onload = function(){
 		}
 		else{ console.log('error');}
 	}
-	//need to find a way to wait 
-
-	console.log(repoNames[5]);
-	var i = 0; 
-	
 	request.send();
+	doWork(); 
 }
 
+function doWork(){
+	setTimeout(testing,60);
+}
 function testing(){
-	for(var i = 0; i< count; i++)
-		document.getElementById("demo").innerHTML += repoNames[i] + "<br>"; 
+	document.getElementById("left1").innerHTML += repoNames[0]+"<br> <img src="+"https://raw.githubusercontent.com/jasont41/"+repoNames[0]+
+"/master/thumbnail.PNG"+" width='80%' height='80%' style='margin-left:10%;margin-right:10%;'>"; 
+	document.getElementById("left2").innerHTML += repoNames[1]; 
+	document.getElementById("left3").innerHTML += repoNames[2]; 
+	//for(var i = 0; i< count; i++)
+	//	document.getElementById("demo").innerHTML += repoNames[i] + "<br>"; 
 }
