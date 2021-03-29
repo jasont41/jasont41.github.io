d4c6ff1b037f152c52f64e5893d2ebf3cac59741
Jason Eastman <jasont41@gmail.com>
2021-03-29 14:47:25 -0700

    frontend changes

--- onloadScript.js
+++ onloadScript.js
@@ -1,9 +1,9 @@
 var repoNames = [];
 var nHTML = '';
-var count = 0; 
+var count = 0;
 window.onload = function(){
-	
-	
+
+
 	fetch('https://api.github.com/users/jasont41/repos')
   .then((response) => {
     return response.json()
@@ -18,22 +18,8 @@ window.onload = function(){
   .catch((err) => {
     // Do something for an error here
   })
-	/*var request = new XMLHttpRequest(); 
-	request.open('GET', 'https://api.github.com/users/jasont41/repos', true) ;
-	request.onload = function(){
-		var data = JSON.parse(this.response)	
-		if (request.status >= 200 && request.status < 400) 
-			{
-				data.forEach((repo) => { 
-				repoNames.push((repo.name)); 
-				console.log(repo.name);
-				count++; 
-				});
-		}
-		else{ console.log('error');}
-	}
-	request.send();*/
-	doWork(); 
+	
+	doWork();
 }
 
 function doWork(){
@@ -41,9 +27,9 @@ function doWork(){
 }
 function testing(){
 	document.getElementById("left1").innerHTML += repoNames[0]+"<br> <img src="+"https://raw.githubusercontent.com/jasont41/"+repoNames[0]+
-"/master/thumbnail.PNG"+" width='80%' height='80%' style='margin-left:10%;margin-right:10%;'>"; 
-	document.getElementById("left2").innerHTML += repoNames[1]; 
-	document.getElementById("left3").innerHTML += repoNames[2]; 
+"/master/thumbnail.PNG"+" width='80%' height='80%' style='margin-left:10%;margin-right:10%;'>";
+	document.getElementById("left2").innerHTML += repoNames[1];
+	document.getElementById("left3").innerHTML += repoNames[2];
 	//for(var i = 0; i< count; i++)
-	//	document.getElementById("demo").innerHTML += repoNames[i] + "<br>"; 
+	//	document.getElementById("demo").innerHTML += repoNames[i] + "<br>";
 }
